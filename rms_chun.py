9c03d9485caf7be276a8ce6c57b467d2fc2eb300
chun-xiao <58152523+chun-xiao@users.noreply.github.com>
2024-04-02 11:54:17 +1100

    Add files via upload
    the supporting package

---
+++ rms_chun.py
@@ -0,0 +1,50 @@
+"""
+Created on Mon Mar 25 16:09:44 2024
+
+@author: Chun
+"""
+import requests
+import json
+import pandas as pd
+
+def rms_ci_query(ci_name_or_id, current = False):
+    page_size = 500
+    if current:
+        query_url = f'''https://dataportal.arc.gov.au/RGS/API/grants?page%5Bnumber%5D=1&page%5Bsize%5D={page_size}&filter=("{ci_name_or_id}")'''
+    else:
+        query_url = f'''https://dataportal.arc.gov.au/NCGP/API/grants?page%5Bnumber%5D=1&page%5Bsize%5D={page_size}&filter=("{ci_name_or_id}")'''
+    r = requests.get(query_url, allow_redirects=True)
+    r_json = json.loads(r.text)
+        
+    ## without status filter here
+    try:
+        grants = []
+        for each in r_json['data']: grants.append(each['attributes'])
+        
+        ##------page loops----------
+        total_pages = r_json['meta']['total-pages']
+        if total_pages > 1:
+            for each_page in range(2, total_pages+1, 1):
+                if current:
+                    query_url = f'''https://dataportal.arc.gov.au/RGS/API/grants?page%5Bnumber%5D={each_page}&page%5Bsize%5D={page_size}&filter=("{ci_name_or_id}")'''
+                else:
+                    query_url = f'''https://dataportal.arc.gov.au/NCGP/API/grants?page%5Bnumber%5D={each_page}&page%5Bsize%5D={page_size}&filter=("{ci_name_or_id}")'''
+                r = requests.get(query_url, allow_redirects=True)
+                r_json = json.loads(r.text)
+                ##----- append grants
+                for each in r_json['data']:  grants.append(each['attributes'])
+        grants = pd.DataFrame.from_records(grants)
+        for drop_col in ['grant-priorities', 'scheme-information', 'researcher_name']:
+            if drop_col in grants.columns:
+                grants = grants.drop(columns = [drop_col], axis =1)
+    
+        grants['searched_by'] = ci_name_or_id
+        if current:
+            grants['searched_DB'] = 'RGS'
+        else:
+            grants['searched_DB'] = 'NCGP'
+    except:
+        grants = pd.DataFrame()
+    return grants
+
+    
